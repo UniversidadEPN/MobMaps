587c623ec1ead0a90b084b46e390ba41219a1b2d
juliorosero1 <rosero.julio.d@gmail.com>
2019-12-18 15:56:47 -0500

    Gran modificacion del códgio y verificar el error

--- app/src/main/java/com/epn/lab11mapsactivityapikey/MapsActivity.kt
+++ app/src/main/java/com/epn/lab11mapsactivityapikey/MapsActivity.kt
@@ -1,45 +1,33 @@
 package com.epn.lab11mapsactivityapikey
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.StreetViewPanoramaFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+//import com.sun.awt.SecurityWarning.setPosition
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+//class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : FragmentActivity(), OnStreetViewPanoramaReadyCallback {
+
 
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
+        val streetViewPanoramaFragment = fragmentManager
+            .findFragmentById(R.id.streetviewpanorama) as StreetViewPanoramaFragment
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this)
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    override fun onStreetViewPanoramaReady(p0: StreetViewPanorama?) {
 
-        mMap.mapType= GoogleMap.MAP_TYPE_SATELLITE
+        p0!!.setPosition(LatLng(-33.87365, 151.20689))
     }
 }
