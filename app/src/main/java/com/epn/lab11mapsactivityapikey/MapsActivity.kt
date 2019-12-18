754f18ad4f6f86c387c0df3be2968eb9f6407379
juliorosero1 <rosero.julio.d@gmail.com>
2019-12-18 15:37:31 -0500

    Tercer commit

--- app/src/main/java/com/epn/lab11mapsactivityapikey/MapsActivity.kt
+++ app/src/main/java/com/epn/lab11mapsactivityapikey/MapsActivity.kt
@@ -39,5 +39,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.mapType= GoogleMap.MAP_TYPE_SATELLITE
     }
 }
